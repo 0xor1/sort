8c2df0821ede76e579932464ca740adee1ee9870
Daniel Robinson <daniel.robinson@autodesk.com>
2015-12-23 14:01:59 +0000

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+sort
+====
+
+a Go package to adapt the sort package from using the sorting Interface object to simply taking the three necessary functions
+
+```sh
+    go get github.com/robsix/sort
+```
---
+++ sort.go
@@ -0,0 +1,31 @@
+package sort
+
+import (
+	"sort"
+)
+
+type sorter struct{
+	len func() int
+	less func(i, j int) bool
+	swap func(i, j int)
+}
+
+func (s *sorter) Len() int {
+	return s.len()
+}
+
+func (s *sorter) Less(i, j int) bool {
+	return s.less(i, j)
+}
+
+func (s *sorter) Swap(i, j int) {
+	return s.swap(i, j)
+}
+
+func Sort(len func() int, less func(i, j int) bool, swap func(i, j int)) {
+	sort.Sort(sorter{
+		len,
+		less,
+		swap,
+	})
+}
